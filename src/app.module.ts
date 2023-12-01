662af7d3de80929232bf59da97ec9213dc987df1
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-12-01 16:53:35 +0000

    dynamic module implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,23 @@ import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {AnatoliaCMSModule} from "../../anatolia-admin-api";
 
+const databaseOptions: any = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'nest-crud',
+    password: 'nest-crud',
+    database: 'nest-crud',
+    autoLoadEntities: true,
+    synchronize: true
+};
+
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'nest-crud',
-            password: 'nest-crud',
-            database: 'nest-crud',
-            autoLoadEntities: true,
-            synchronize: true
-        }),
-        AnatoliaCMSModule.register()
+        TypeOrmModule.forRoot(databaseOptions),
+        AnatoliaCMSModule.register({
+            database: databaseOptions
+        })
     ],
     controllers: [AppController],
     providers: [AppService],
