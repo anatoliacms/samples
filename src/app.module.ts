8dd96b64b8825edd5702de56eb2dbebcf6ef0e9e
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-12-01 16:30:02 +0000

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
 import {Module} from '@nestjs/common';
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {imports} from "./module.definition";
-import {AnatoliaCMSModule} from "@anatoliacms/anatolia-core";
+import {AnatoliaCMSModule} from "../../anatolia-admin-api";
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'nest-crud',
-    password: 'nest-crud',
-    database: 'nest-crud',
-    autoLoadEntities: true,
-    synchronize: true
-  }),
-    ...imports,
-    AnatoliaCMSModule.register()
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'nest-crud',
+            password: 'nest-crud',
+            database: 'nest-crud',
+            autoLoadEntities: true,
+            synchronize: true
+        }),
+        AnatoliaCMSModule.register()
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/constant.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_PROJECT_PATH = './projects';
-export const DEFAULT_SOURCE_PATH = 'src';
