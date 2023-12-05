2ca1caa1596643e7b410df2552abe9497cc27f54
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-12-05 23:37:23 +0000

    common & cli implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,29 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {AnatoliaCMSModule} from "../../anatolia-admin-api";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnatoliaCMSModule } from '../../anatolia-admin-api';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 const databaseOptions: any = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'nest-crud',
-    password: 'nest-crud',
-    database: 'nest-crud',
-    autoLoadEntities: true,
-    synchronize: true
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'nest-crud',
+  password: 'nest-crud',
+  database: 'nest-crud',
+  autoLoadEntities: true,
+  synchronize: true,
 };
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot(databaseOptions),
-        AnatoliaCMSModule.register({
-            database: databaseOptions
-        })
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(databaseOptions),
+    AnatoliaCMSModule.register({
+      database: databaseOptions,
+      workingDirectory: './projects',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
